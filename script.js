be07650b6ac40861264a6b6e5a03d0032473b5f7
Miles Perkins <mperkins1993@gmail.com>
2015-07-17 15:28:01 -0400

    winning conditions moving along

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 console.log("linked!");
 
 //set up gameboard so that each box has its own id.
-//match up box id with each index value in an array.
-//add event listener for each box.
 
 //This takes the parent element and adds the event listener which essentially trickles down to the child elements.
 
@@ -22,16 +20,21 @@ var turnCount = 1;
     //$(player).stopPropagation();
       };
     };
-
     console.log(event);
     console.log(turnCount);
     turnCount++;
+
+    var win1 = document.querySelector('#box0').innerText === document.querySelector('#box1').innerText && document.querySelector('#box1').innerText === document.querySelector('#box2').innerText;
+    var win2 = document.querySelector('#box3').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box5').innerText;
+    var win3 = document.querySelector('#box6').innerText === document.querySelector('#box7').innerText && document.querySelector('#box7').innerText === document.querySelector('#box8').innerText;
+    var win4 = document.querySelector('#box0').innerText === document.querySelector('#box3').innerText && document.querySelector('#box3').innerText === document.querySelector('#box6').innerText;
+    var win5 = document.querySelector('#box1').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box7').innerText;
+    var win6 = document.querySelector('#box2').innerText === document.querySelector('#box5').innerText && document.querySelector('#box5').innerText === document.querySelector('#box8').innerText;
+    var win6 = document.querySelector('#box2').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box6').innerText;
+    var win6 = document.querySelector('#box0').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box8').innerText;
+  
+  if (player = win1) {
+    console.log("winner!");
+  }
+
   });
-
-  //if a box is not undefined,
-  //then, cannot target event.
-
-  // var winCombo = function() {
-  //   if 
-  // }
-
