999acff3b9da734f09050490710c8c789dd31325
Miles Perkins <mperkins1993@gmail.com>
2015-07-18 19:11:57 -0400

    Turn count adjusted as to not mess up which symbol goes

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ console.log("linked!");
 
 //Creating the players essentially by turnCount. Each turn will cause the box innerText to be either and "X" or an "O".
 var turnCount = 1;
+var singlePlayerButton = $('.mode1');
+var multiPlayerButton = $('.mode2');
+var computerMove = false;
+
+
   $('#container').on('click', function(event){
     var player = event.target; //the target of the event are the boxes.
     if (player.innerText === "") { 
@@ -15,26 +20,44 @@ var turnCount = 1;
     //$(player).stopPropagation();
       }
       else if (turnCount % 2 === 0) { //Even turns are "O".
+        var player = event.target
         player.innerText = "O";
 
     //$(player).stopPropagation();
-      };
+      }
+    }
+    else if (player != "") {
+      turnCount--; //This stops the turnCount from continuing upwards which would disrupt the order of "X" and "O".
     };
+
     console.log(event);
     console.log(turnCount);
     turnCount++;
 
+
+  //Redo this as if else statements instead of using "||".
+  });
+
+  var winConditions = function() {
+    
     var win1 = document.querySelector('#box0').innerText === document.querySelector('#box1').innerText && document.querySelector('#box1').innerText === document.querySelector('#box2').innerText;
     var win2 = document.querySelector('#box3').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box5').innerText;
     var win3 = document.querySelector('#box6').innerText === document.querySelector('#box7').innerText && document.querySelector('#box7').innerText === document.querySelector('#box8').innerText;
     var win4 = document.querySelector('#box0').innerText === document.querySelector('#box3').innerText && document.querySelector('#box3').innerText === document.querySelector('#box6').innerText;
     var win5 = document.querySelector('#box1').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box7').innerText;
     var win6 = document.querySelector('#box2').innerText === document.querySelector('#box5').innerText && document.querySelector('#box5').innerText === document.querySelector('#box8').innerText;
-    var win6 = document.querySelector('#box2').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box6').innerText;
-    var win6 = document.querySelector('#box0').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box8').innerText;
-  
-  if (player = win1) {
-    console.log("winner!");
-  }
+    var win7 = document.querySelector('#box2').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box6').innerText;
+    var win8 = document.querySelector('#box0').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box8').innerText;
 
-  });
+    if (player1 = win1) {
+      alert("Player 1 wins!");
+    }
+  };
+
+
+
+
+
+
+
+
