884fa6c3582a276240ad2cdd58aa178cd4eed226
Miles Perkins <mperkins1993@gmail.com>
2015-07-20 11:24:05 -0400

    finished win conditions

---
+++ README.md
@@ -0,0 +1,16 @@
+This game is Tic Tac Toe.
+
+This game is operated via turn count which switch the symbols utilized every turn.
+
+Odd turns are "X", while even turns are "O".
+
+Game rules:
+
+In order to play you need to select single player or multiplayer.
+
+If single player, you will play an AI.
+
+
+
+
+
--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var singlePlayerButton = $('.mode1');
 var multiPlayerButton = $('.mode2');
 var computerMove = false;
 
-
+$('.mode2').on('click', function() {
   $('#container').on('click', function(event){
     var player = event.target; //the target of the event are the boxes.
     if (player.innerText === "") { 
@@ -20,7 +20,6 @@ var computerMove = false;
     //$(player).stopPropagation();
       }
       else if (turnCount % 2 === 0) { //Even turns are "O".
-        var player = event.target
         player.innerText = "O";
 
     //$(player).stopPropagation();
@@ -34,26 +33,84 @@ var computerMove = false;
     console.log(turnCount);
     turnCount++;
 
-
-  //Redo this as if else statements instead of using "||".
+    // var winConditions = function() {
+      /* Horizontal
+      var win1 = document.querySelector('#box0').innerText === document.querySelector('#box1').innerText && document.querySelector('#box1').innerText === document.querySelector('#box2').innerText;
+      var win2 = document.querySelector('#box3').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box5').innerText;
+      var win3 = document.querySelector('#box6').innerText === document.querySelector('#box7').innerText && document.querySelector('#box7').innerText === document.querySelector('#box8').innerText;
+      */
+      /* Vertical
+      var win4 = document.querySelector('#box0').innerText === document.querySelector('#box3').innerText && document.querySelector('#box3').innerText === document.querySelector('#box6').innerText;
+      var win5 = document.querySelector('#box1').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box7').innerText;
+      var win6 = document.querySelector('#box2').innerText === document.querySelector('#box5').innerText && document.querySelector('#box5').innerText === document.querySelector('#box8').innerText;
+      */
+      /* Diagnol
+      var win7 = document.querySelector('#box2').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box6').innerText;
+      var win8 = document.querySelector('#box0').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box8').innerText;
+      */
+      console.log("player",player.innerText);
+      if (document.querySelector('#box0').innerText != "" && document.querySelector('#box1').innerText != "" && document.querySelector('#box2').innerText != "") {
+        if(document.querySelector('#box0').innerText === document.querySelector('#box1').innerText && document.querySelector('#box1').innerText === document.querySelector('#box2').innerText) {
+          alert("Winner! Top Row");
+        }
+      }
+      else if (document.querySelector('#box3').innerText != "" && document.querySelector('#box4').innerText != "" && document.querySelector('#box5').innerText != "") {
+        if (document.querySelector('#box3').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box5').innerText) {
+          alert("winner! second row");
+        }
+      }
+      else if (document.querySelector('#box6').innerText != "" && document.querySelector('#box7').innerText != "" && document.querySelector('#box8').innerText != "") {
+        if (document.querySelector('#box6').innerText === document.querySelector('#box7').innerText && document.querySelector('#box7').innerText === document.querySelector('#box8').innerText) {
+          alert("winner!");
+        }
+      }
+      else if (document.querySelector('#box0').innerText != "" && document.querySelector('#box3').innerText != "" && document.querySelector('#box6').innerText != "") {
+        if (document.querySelector('#box0').innerText === document.querySelector('#box3').innerText && document.querySelector('#box3').innerText === document.querySelector('#box6').innerText) {
+          alert("winner!");
+        }
+      }
+      else if (document.querySelector('#box1').innerText != "" && document.querySelector('#box4').innerText != "" && document.querySelector('#box7').innerText != "") {
+        if (document.querySelector('#box1').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box7').innerText) {
+          alert("winner!");
+        }        
+      }
+      else if (document.querySelector('#box2').innerText != "" && document.querySelector('#box5').innerText != "" && document.querySelector('#box8').innerText != "") {
+        if (document.querySelector('#box2').innerText === document.querySelector('#box5').innerText && document.querySelector('#box5').innerText === document.querySelector('#box8').innerText) {
+          alert("winner!");
+        }
+      }
+      else if (document.querySelector('#box2').innerText != "" && document.querySelector('#box4').innerText != "" && document.querySelector('#box6').innerText != "") {
+        if (document.querySelector('#box2').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box6').innerText) {
+          alert("winner!");
+        }
+      }
+      else if (document.querySelector('#box0').innerText != "" && document.querySelector('#box4').innerText != "" && document.querySelector('#box8').innerText != "") {
+        if (document.querySelector('#box0').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box8').innerText) {
+          alert("winner!");
+        }
+      }
+      //Here the win conditions are set. They cover all horizontal, vertical, and diagnol combinations.
   });
+      
+});
 
-  var winConditions = function() {
-    
-    var win1 = document.querySelector('#box0').innerText === document.querySelector('#box1').innerText && document.querySelector('#box1').innerText === document.querySelector('#box2').innerText;
-    var win2 = document.querySelector('#box3').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box5').innerText;
-    var win3 = document.querySelector('#box6').innerText === document.querySelector('#box7').innerText && document.querySelector('#box7').innerText === document.querySelector('#box8').innerText;
-    var win4 = document.querySelector('#box0').innerText === document.querySelector('#box3').innerText && document.querySelector('#box3').innerText === document.querySelector('#box6').innerText;
-    var win5 = document.querySelector('#box1').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box7').innerText;
-    var win6 = document.querySelector('#box2').innerText === document.querySelector('#box5').innerText && document.querySelector('#box5').innerText === document.querySelector('#box8').innerText;
-    var win7 = document.querySelector('#box2').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box6').innerText;
-    var win8 = document.querySelector('#box0').innerText === document.querySelector('#box4').innerText && document.querySelector('#box4').innerText === document.querySelector('#box8').innerText;
+$('.mode1').on('click', function() {
+  $('#container').on('click', function(event) {
+    var player = event.target
+    if (player.innerText === "") {
+      if (turnCount % 2 != 0) {
+        player.innerText = "X";
+      }
+    };
 
-    if (player1 = win1) {
-      alert("Player 1 wins!");
-    }
-  };
+  });
+});
 
+
+
+//Fix winning conditions so that they actually work
+//Set up AI
+//CSS
 
 
 
